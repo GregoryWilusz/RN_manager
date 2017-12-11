648540a611b9a6ebe26c9fba7a7b2043654e89d1
Grzegorz Wilusz <grzegorzwilusz@MacBook-Air-Grzegorz.local>
2017-12-11 10:23:50 +0100

    Finished setup.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: "AIzaSyCGQdPMoSbZIb4ylJFOr1v9XzWUmdT5lSk",
+            authDomain: "manager-ff351.firebaseapp.com",
+            databaseURL: "https://manager-ff351.firebaseio.com",
+            projectId: "manager-ff351",
+            storageBucket: "manager-ff351.appspot.com",
+            messagingSenderId: "1061204707926"
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
-            <Provider store={createStore()}>
+            <Provider store={createStore(reducers)}>
                 <View>
                     <Text>
                         Hello!
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    banana: () => []    // dummy default reducer which always returns an empty array
+});
